c7e8a643880b5a50217891e86454b4e8abde4513
Kinjalk <kinjalkbajpai@gmail.com>
2024-03-23 01:29:23 +0530

    added typescript and express

--- backend/index.ts
+++ backend/index.ts
@@ -1,24 +1,25 @@
-const { Pool } = require('pg')
+import pg from "pg";
+const { Pool } = pg;
+import 'dotenv/config';
 
-require('dotenv').config()
 
 let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 
 const pool = new Pool({
     host: PGHOST,
     database: PGDATABASE,
-    username: PGUSER,
+    user: PGUSER,
     password: PGPASSWORD,
     port: 5432,
     ssl: {
         require: true,
-    },
+    } as any,
 })
 
 async function getPgVersion() {
     const client = await pool.connect();
     try {
-        const result = await client.query('SELECT version()');
+        const result= await client.query('SELECT version()');
         console.log(result.rows[0]);
     } finally {
         client.release();
